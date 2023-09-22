2b38dbd2afa481c5b7801edd83f52d915a0dbaa1
pvprasad789 <pvpindian@gmail.com>
2023-09-22 08:21:46 +0530

    DBC

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+import './custom.scss';
+// import $ from 'jquery';
+// import Popper from 'popper.js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import "bootstrap-icons/font/bootstrap-icons.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
